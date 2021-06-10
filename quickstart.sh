ee0858990f64f5e743e041dac0c30b1a916610f3
Jeff Fletcher <fletch.jeff@gmail.com>
2021-06-10 12:25:55 +0200

    ssh-agent fix

--- quickstart.sh
+++ quickstart.sh
@@ -63,6 +63,15 @@ fi
 echo "Mounting ${PROJECT_DIR} to container as Project Directory /runner/project"
 echo "Creating Container ${CONTAINER_NAME} from image ${IMAGE_FULL_NAME}"
 
+echo "Checking ssh-agent is running for Windows"
+if pgrep -x "ssh-agent" >/dev/null
+then
+    echo "ssh-agent is running"
+else
+    echo "ssh-agent stopped, starting it"
+    eval `ssh-agent -s` 
+fi
+
 echo "Checking OS"
 if [ ! -f "/run/host-services/ssh-auth.sock" ]; 
 then
